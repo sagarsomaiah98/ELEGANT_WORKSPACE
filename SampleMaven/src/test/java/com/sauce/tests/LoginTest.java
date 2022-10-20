203dbc7f28fcfa5c2b4a21a22a0ca591de2465c5
Som <somaiah1988@gmail.com>
2022-10-20 10:53:28 -0400

    java

--- SampleMaven/src/test/java/com/sauce/tests/LoginTest.java
+++ SampleMaven/src/test/java/com/sauce/tests/LoginTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
